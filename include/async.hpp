2ed36779a0a0cb85131cf4d604b18c4a8091f0fe
Vladlen Naumov <naumov.vlad04@gmail.com>
2026-05-15 14:25:43 +0300

    Format

--- include/async.hpp
+++ include/async.hpp
@@ -3,114 +3,104 @@
 int counter = 0;
 
 template <typename T>
-requires (!std::is_void_v<T>)
+  requires(!std::is_void_v<T>)
 struct Task {
-    struct promise_type;
-    using coro_handle_t = std::coroutine_handle<promise_type>;
+  struct promise_type;
+  using coro_handle_t = std::coroutine_handle<promise_type>;
 
-    struct promise_type {
-        auto get_return_object() -> coro_handle_t {
-            return coro_handle_t::from_promise(*this);
-        }
-        
-        std::suspend_never initial_suspend() noexcept {
-            return {};
-        }
-        
-        std::suspend_always final_suspend() noexcept {
-            return {};
-        }
-        
-        void return_value(T value) {
-            std::print("Calling return_value in promise_object\n");
-            this->value = value;
-        }
-        
-        void unhandled_exception() {
-            std::print("Unhandled exception caught in the coroutine!\n"); 
-            std::terminate();
-        }
-
-        T value;
-    };
-
-    Task() {
-        id = counter++;
-        std::print("Default Task {} ctor\n", id);
+  struct promise_type {
+    auto get_return_object() -> coro_handle_t {
+      return coro_handle_t::from_promise(*this);
     }
 
-    Task(coro_handle_t handle) : coro_handle( handle) {
-        id = counter++;
-        std::print("Task {} has been created from coro_handle\n", id);
+    std::suspend_never initial_suspend() noexcept { return {}; }
+
+    std::suspend_always final_suspend() noexcept { return {}; }
+
+    void return_value(T value) {
+      std::print("Calling return_value in promise_object\n");
+      this->value = value;
     }
 
-    Task(const Task<T>&) = delete;
-    Task(Task<T>&& other) : coro_handle(other.coro_handle) {
-        id = counter++;
-        std::print("Move Task {} ctor\n", id);
-        other.coro_handle = nullptr;
+    void unhandled_exception() {
+      std::print("Unhandled exception caught in the coroutine!\n");
+      std::terminate();
     }
 
-    Task<T>& operator=(const Task<T>&) = delete;
-    Task<T>& operator=(Task<T>&& other) {
-        std::print("Move Task {} assignment operator\n", id);
-        if (coro_handle) {
-            coro_handle.destroy();
-            coro_handle = other.coro_handle;
-            other.coro_handle = nullptr;
-        } else {
-            coro_handle = other.coro_handle;
-            other.coro_handle = nullptr;
-        }
-        return *this;
-    }
+    T value;
+  };
 
-    ~Task() {
-        std::print("Destoying Task {}\n", id);
-        if(coro_handle) {
-            coro_handle.destroy();
-        }
-    }
-    
-    T get_value() {
-        return coro_handle.promise().value;
-    }
+  Task() {
+    id = counter++;
+    std::print("Default Task {} ctor\n", id);
+  }
 
-    coro_handle_t coro_handle;
-    int id = -1;
+  Task(coro_handle_t handle) : coro_handle(handle) {
+    id = counter++;
+    std::print("Task {} has been created from coro_handle\n", id);
+  }
+
+  Task(const Task<T> &) = delete;
+  Task(Task<T> &&other) : coro_handle(other.coro_handle) {
+    id = other.id;
+    std::print("Move Task {} ctor\n", id);
+    other.coro_handle = nullptr;
+  }
+
+  Task<T> &operator=(const Task<T> &) = delete;
+  Task<T> &operator=(Task<T> &&other) {
+    std::print("Move Task {} assignment operator\n", id);
+    if (coro_handle) {
+      coro_handle.destroy(); // this destroys the frame
+                             // YOU CANNOT RESUME DESTROYED FRAMES
+                             // suspend_always because you allow moves
+                             // you can't not suspend or else UB
+    }
+    coro_handle = other.coro_handle;
+    other.coro_handle = nullptr;
+    id = other.id;
+    return *this;
+  }
+
+  ~Task() {
+    std::print("Destoying Task {}\n", id);
+    if (coro_handle) {
+      coro_handle.destroy();
+    }
+  }
+
+  T get_value() { return coro_handle.promise().value; }
+
+  coro_handle_t coro_handle;
+  int id = -1;
 };
 
-
-
 template <typename T>
-requires (!std::is_void_v<T>)
+  requires(!std::is_void_v<T>)
 struct Awaitable {
-    struct Awaiter {
-        Awaiter(T value) : value(value) {}
+  struct Awaiter {
+    Awaiter(T value) : value(value) {}
 
-        bool await_ready() {
-            return false;
-        }
+    bool await_ready() { return false; }
 
-        void await_suspend(Task<T>::coro_handle_t handle) noexcept {
-            std::print("Setting coro_handle and suspend. tID: {}\n", std::this_thread::get_id());
-        }
-
-        T await_resume() noexcept {
-            std::print("Resuming coroutine. tID: {}\n", std::this_thread::get_id());
-            return value;
-        }
-
-    private:
-        T value;
-    };
-
-    Awaitable(T value) : m_awaiter(value) {}
-
-    Awaiter operator co_await() {
-        return m_awaiter;
+    void await_suspend(Task<T>::coro_handle_t handle) noexcept {
+      std::print("Setting coro_handle and suspend. tID: {}\n",
+                 std::this_thread::get_id());
     }
 
+    T await_resume() noexcept {
+      std::print("Resuming coroutine. tID: {}\n", std::this_thread::get_id());
+      return value;
+    }
+
+  private:
+    T value;
+  };
+
+  Awaitable(T value) : m_awaiter(value) {}
+
+  Awaiter operator co_await() { return m_awaiter; }
+
 private:
-    Awaiter m_awaiter;
+  Awaiter m_awaiter;
 };
--- include/consumer.hpp
+++ include/consumer.hpp
@@ -3,36 +3,46 @@
 #include <async.hpp>
 
 struct Request {
-    char* buffer{nullptr};
-    size_t buffer_size{0};
-    Task<Request*> task;
-    std::atomic_bool is_ready{false};
-    bool is_last{false};
+  char *buffer{nullptr};
+  size_t buffer_size{0};
+  Task<Request *> task;
+  std::atomic_bool is_ready{false};
+  bool is_last{false};
 
-    Request() = default;
-    Request(const Request&) = delete;
-    Request(Request&&) = default;
+  Request() = default;
+  Request(const Request &) = delete;
+  Request(Request &&other) {
+    buffer = other.buffer;
+    other.buffer = nullptr;
+    buffer_size = other.buffer_size;
+    other.buffer_size = 0;
+    task = std::move(other.task);
+    // TODO: think about memory order
+    is_ready = other.is_ready.load();
+    is_last = other.is_last;
+  };
 };
 
-
 class Consumer {
-    Task<Request*> deferred_consume(Request* req) {
-        // async wait for request to be resumed in processor
-        Request* response = co_await Awaitable(req);
-        std::string_view result{response->buffer, response->buffer_size};
-        std::print("Response is: {}. tId: {}\n", result, std::this_thread::get_id());
-        delete response;
-        co_return nullptr; // crutch
-    }
+  Task<Request *> deferred_consume(Request *req) {
+    // async wait for request to be resumed in processor
+    Request *response = co_await Awaitable(req);
+    std::string_view result{response->buffer, response->buffer_size};
+    std::print("Response is: {}. tId: {}\n", result,
+               std::this_thread::get_id());
+    delete response;
+    co_return nullptr; // crutch
+  }
+
 public:
-    Request* create_request() {
-        Request *req = new Request;
-        req->buffer = new char[m_response_size];
-        req->buffer_size = m_response_size;
-        req->task = deferred_consume(req);
-        return req;
-    }
+  Request *create_request() {
+    Request *req = new Request;
+    req->buffer = new char[m_response_size];
+    req->buffer_size = m_response_size;
+    req->task = deferred_consume(req);
+    return req;
+  }
 
 private:
-    static constexpr const size_t m_response_size = 10;
+  static constexpr const size_t m_response_size = 10;
 };
